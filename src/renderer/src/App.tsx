bd6c2cd1421dd647725254aaca5da96e2eec693c
MdRezaV <mdrezav@gmail.com>
2026-07-26 15:22:39 +0330

    fix directory structure excluded on repeated copies

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -486,7 +486,7 @@ function App(): React.JSX.Element {
 
         // 3. Prepare context and copy
         let dirStructure: string | undefined
-        if (currentDirStructureTag !== null && currentDirStructureTag !== 'ADD') {
+        if (currentDirStructureTag !== null) {
           dirStructure = await window.electron.ipcRenderer.invoke('read-directory-tree', workspace)
           if (currentDirStructureTag === 'PND') {
             setDirStructureTag('INQ')
